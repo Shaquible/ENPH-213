527c0f3b7af26ceff5afa1eefee508cea4a7d97f
John Doe <keegkelly2013@gmail.com>
2022-03-12 19:40:01 -0500

    done

--- Lab 6/Lab6_Kelly.py
+++ Lab 6/Lab6_Kelly.py
@@ -30,7 +30,7 @@ def IDFT(ck, n, tmin=0, tmax=2*np.pi):
     k = np.linspace(0, n-1, n, endpoint=True)
     j = np.linspace(0, n-1, n, endpoint=True)
     # calculating the IDFT according to equation 13
-    yj = 1/n*np.sum(ck*np.exp(1j*k*((tmax-tmin)/n*j[:, None]+tmin)), axis=1)
+    yj = 1/n*np.sum(ck*np.exp(1j*k*2*np.pi/n*j[:, None]), axis=1)
     return (tmax-tmin)*j/n+tmin, yj
 
 
@@ -125,14 +125,13 @@ def DFTvariableTime(f, n, tmin, tmax, w, sigma):
     if n % 2 == 1:
         print("n must be even")
         return 0, 0
-    deltat = (tmax-tmin)/n
     # this j array will be used to generate the time values and then used as Ks in the sum
     j = np.linspace(0, n-1, n, endpoint=True)
-    tj = j*deltat+tmin
+    tj = j*(tmax-tmin)/n+tmin
 
     yj = f(tj, w, sigma)
     # summing across all time values for each k (now in the j array)
-    ck = np.sum(yj*np.exp(-1j*j[:, None]*tj), axis=1)
+    ck = np.sum(yj*np.exp(-1j*j[:, None]*j*2*np.pi/n), axis=1)
     return j, ck
 
 
@@ -141,14 +140,13 @@ def DFTshift(f, n, tmin, tmax, w, sigma):
     if n % 2 == 1:
         print("n must be even")
         return 0, 0
-    deltat = (tmax-tmin)/n
     j = np.linspace(0, n-1, n, endpoint=True)
-    tj = j*deltat+tmin
+    tj = j*(tmax-tmin)/n+tmin
 
     yj = f(tj, w, sigma)
-    ck = np.sum(yj*np.exp(-1j*j[:, None]*tj), axis=1)
+    ck = np.sum(yj*np.exp(-1j*j[:, None]*j*2*np.pi/n), axis=1)
     # shifting according to the code given on the lecture slides
-    wshift = np.fft.fftshift(np.fft.fftfreq(n, deltat)*2*np.pi)
+    wshift = np.fft.fftshift(np.fft.fftfreq(n, (tmax-tmin)/n)*2*np.pi)
     yshift = np.fft.fftshift(ck)
     return wshift, yshift
 
@@ -179,13 +177,13 @@ fig, ax = plt.subplots(1, 2, figsize=(10, 5))
 ax[0].plot(t, yj, "r-", label="$\omega_p=10$")
 ax[0].set_xlabel("t")
 ax[0].set_ylabel("y(t)")
-ax[1].plot(wshift, abs(yshift), "r-", label="$\omega_p=10$")
+ax[1].plot(wshift, abs(yshift), "ro-", label="$\omega_p=10$")
 ax[1].set_xlabel("$\omega$")
 ax[1].set_ylabel(r"$|\tilde{y}|$")
 wshift, yshift = DFTshift(GaussianPulse, 400, -np.pi, np.pi, 20, 1)
 yj = GaussianPulse(t, 20, 1)
 ax[0].plot(t, yj, "b--", label="$\omega_p=20$")
-ax[1].plot(wshift, abs(yshift), "b--", label="$\omega_p=20$")
+ax[1].plot(wshift, abs(yshift), "bo-", label="$\omega_p=20$")
 ax[1].set_xlim(-40, 40)
 ax[1].legend()
 plt.show()
@@ -196,15 +194,17 @@ t = np.linspace(-np.pi, np.pi, 800)
 yj = GaussianPulse(t, 10, 1)
 fig, ax = plt.subplots(1, 2, figsize=(10, 5))
 ax[0].plot(t, yj, "r-", label="$\omega_p=10$")
+ax[0].set_title("Double Frequency Resolution")
 ax[0].set_xlabel("t")
 ax[0].set_ylabel("y(t)")
-ax[1].plot(wshift, abs(yshift), "r-", label="$\omega_p=10$")
+ax[1].plot(wshift, abs(yshift), "ro-", label="$\omega_p=10$")
 ax[1].set_xlabel("$\omega$")
 ax[1].set_ylabel(r"$|\tilde{y}|$")
 wshift, yshift = DFTshift(GaussianPulse, 800, -2*np.pi, 2*np.pi, 20, 1)
 yj = GaussianPulse(t, 20, 1)
+ax[1].set_xlim(-40, 40)
 ax[0].plot(t, yj, "b--", label="$\omega_p=20$")
-ax[1].plot(wshift, abs(yshift), "b--", label="$\omega_p=20$")
+ax[1].plot(wshift, abs(yshift), "bo-", label="$\omega_p=20$")
 ax[1].legend()
 plt.show()
 
@@ -215,36 +215,32 @@ def y1(t):
     return 3*np.sin(t)+np.sin(10*t)
 
 
-def DFTvariableTimeY1(f, n, tmin, tmax):
+def DFTvariableTimeY1(f, n, tmin, tmax, ncycles):
     # same DFT variable time array as before but does have the other variables to call the pulse fn
     if n % 2 == 1:
         print("n must be even")
         return 0, 0
-    deltat = (tmax-tmin)/n
-    j = np.linspace(0, n-1, n, endpoint=True)
-    tj = j*deltat+tmin
-
+    j = np.linspace(0, n-1, n, endpoint=True, dtype=float)
+    tj = j*(tmax-tmin)/n+tmin
     yj = f(tj)
-    ck = np.sum(yj*np.exp(-1j*j[:, None]*tj), axis=1)
+    ck = np.sum(yj*np.exp(-1j*j[:, None]*j*2*np.pi/n), axis=1)
+    j = j/ncycles
     return j, ck
 
 
-def freqFilter(w0, w, ck):
+def freqFilter(w, ck):
     # searching for the most significant frequency and turning all others to 0 to eliminate noise
     # finding the most significant frequency component
     max = abs(ck).max()
-    found = False
-    # searching for the first index to have roughly the max magnitude so the frequency chosen is in the first cycle
+    # searching for indexs that have roughly the magnitude as max to account for floating point errors
     for i in range(len(w)):
-        if round(abs(ck[i])) != round(max) or found:
+        if round(abs(ck[i])) != round(max):
             ck[i] = 0
-        else:
-            found = True
     return ck
 
 
 # calculating the dft and plotting it and the original function
-k, ck = DFTvariableTimeY1(y1, 200, 0, 8*np.pi)
+k, ck = DFTvariableTimeY1(y1, 200, 0, 8*np.pi, 4)
 t = np.linspace(0, 8*np.pi, 200)
 yj = y1(t)
 fig, ax = plt.subplots(1, 2, figsize=(10, 5))
@@ -254,11 +250,11 @@ ax[0].set_ylabel("y(t)")
 ax[1].plot(k, abs(ck), "r-", label="filter")
 ax[1].set_xlabel("$\omega$")
 ax[1].set_ylabel(r"$|\tilde{y}|$")
-ax[1].set_xlim(0, 50)
+#ax[1].set_xlim(0, 50)
 # filtering the frequencies and plotting them
-ck = freqFilter(1, k, ck)
+ck = freqFilter(k, ck)
 # calculating the IDFT with the filtered frequencies and plotting it
 xs, ys = IDFT(ck, 200, 0, 8*np.pi)
-ax[0].plot(xs, ys, "b--", label="filtered")
+ax[0].plot(xs, np.real(ys), "b-", label="filtered")
 ax[1].plot(k, abs(ck), "b-", label="filtered")
 plt.show()
