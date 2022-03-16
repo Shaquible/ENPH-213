d486be028b98f7bfb8a6116b114531ca26090614
Keegan Kelly <keegkelly2013@gmail.com>
2022-03-16 11:07:29 -0400

    fdsfs

--- convolver.m
+++ convolver.m
@@ -1,11 +1,17 @@
 function y = convolver(h, x)
 %convolver give two arrays h and x to be convolved
+
+    %resphing the arrays to convert any column vectors to row vectors
     h = reshape(h, 1, []);
-    x = reshape(x, 1, []);   
+    x = reshape(x, 1, []);
+    %calculating number of non-zero elements in the convolution   
     N = length(h) + length(x) - 1;
+    %filling the output array with zeros
     y = zeros(1, N);
+    %resizing the input array with zeros to be the size length as y
     x(numel(y)) = 0;
     h(numel(y)) = 0;
+    %calculating the convolution
     for n = 1:N
         i = 1;
         for k = n:-1:1
@@ -13,12 +19,14 @@ function y = convolver(h, x)
             i = i + 1;    
         end
     end
-    tiledlayout(3,1);
-    ax1 = nexttile;
-    title(x(n));
-    ax2 = nexttile;
-    ax3 = nexttile;
-    stem(ax2, x);
-    stem(ax1, h);
-    stem(ax3, y);
+    %plotting
+    subplot(3,1,1)
+    stem(x)
+    title('x(n)')
+    subplot(3,1,2)
+    stem(h)
+    title('h(n)')
+    subplot(3,1,3)
+    stem(y)
+    title('y(n)')
 end
